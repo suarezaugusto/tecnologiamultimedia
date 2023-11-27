62ac2c818fb1f2b6e6548296d3fbb20e6f78e121
suarezaugusto <128536522+suarezaugusto@users.noreply.github.com>
2023-11-27 17:24:13 -0300

    Add files via upload

---
+++ class_Asteroides.js
@@ -0,0 +1,36 @@
+class Asteroides {
+  constructor(x, y, tam) {
+    this.x = x;
+    this.y = y;
+    this.V = random(1.5, 2.9); 
+    this.tam = tam;  
+    this.vida = true;
+    this.bala = new Bala();
+    this.impacto = true; 
+  }
+  
+  dibujar(){
+    if(this.vida){
+      fill(0, 255, 0);
+      ellipse(this.x, this.y, this.tam);
+      this.movimiento();
+    }
+  }
+  movimiento(){
+    this.y += this.V;
+    if(this.y>height){
+      this.y = -this.tam;
+  //  this.impacto = true;
+    }
+  }
+  matar(){
+    this.vida = false;
+  }
+  
+  haTocadoLaBala(bala){
+    if(dist(this.x, this.y, bala.Bx, bala.By) < 15 ){
+      this.matar(); 
+   }
+ }
+
+}
